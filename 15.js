371e67b3f66f19c980a0a6e2632d5b057cf2d6e1
Carlos <carlos@marah.dev>
2021-12-16 22:50:25 +0100

    Better problem 15 code

--- 15.js
+++ 15.js
@@ -1,72 +1,73 @@
-// A
+// Setup
 const lines = document.body.innerText.split('\n').filter(x => x);
-const map = (new Array(lines.length)).fill(0)
-  .map((row, i) => lines[i].split('').map(x => parseInt(x)));
 
-const findPathWithCost = (running_cost, map, next_nodes, done) => {
-  const valid_next_nodes_with_cost = (next_nodes[running_cost] || [])
-    .filter(node => !done[node.split('_')[0]][node.split('_')[1]]);
-  if (valid_next_nodes_with_cost.includes(`${map.length-1}_${map[0].length-1}`)) { // Found exit
-    return running_cost;
-  }
-  if (!valid_next_nodes_with_cost.length) { // No available nodes to move through, increase cost
-    return findPathWithCost(running_cost+1, map, next_nodes, done);
-  }
-  const new_next_nodes = valid_next_nodes_with_cost.reduce((nn, node) => {
-    const node_x = parseInt(node.split('_')[0]);
-    const node_y = parseInt(node.split('_')[1]);
+const alreadyHasNode = (node_array, [pos_x, pos_y]) => (node_array || []).some(
+  ([ node_x, node_y ]) => node_x === pos_x && node_y === pos_y
+);
+
+const addFoundNodes = (map_cost_nodes, next_nodes, cave, done, running_cost) =>
+  // Add adjacent nodes to cost_nodes object;  { [cost]: [nodes with cost] }
+  next_nodes.reduce((new_map_cost_nodes, [node_x, node_y]) => {
     const possible_movements = [
       [node_x,   node_y-1],
       [node_x,   node_y+1],
       [node_x-1, node_y  ],
       [node_x+1, node_y  ],
-    ];
+    ].filter(([move_x, move_y]) =>
+      // check move node is inside cave & has not been done
+      0 <= move_x && move_x < cave.length &&
+      0 <= move_y && move_y < cave.length &&
+      !done[move_x][move_y]
+    );
     possible_movements.forEach(([move_x, move_y]) => {
-      if (
-        0 <= move_x && move_x < map.length &&
-        0 <= move_y && move_y < map.length &&
-        !done[move_x][move_y]
-      ) {
-        const move_cost = running_cost + map[move_x][move_y];
-        if (!nn[move_cost] || !nn[move_cost].includes(`${move_x}_${move_y}`)) {
-          nn[move_cost] = (nn[move_cost] || []).concat(`${move_x}_${move_y}`);
-        }
+      // if we don't already have move with that cost, add it
+      const move_cost = running_cost + cave[move_x][move_y];
+      if (!alreadyHasNode(new_map_cost_nodes[move_cost], [move_x, move_y])) {
+        new_map_cost_nodes[move_cost] = (new_map_cost_nodes[move_cost] || [])
+          .concat([[move_x, move_y]]);
       }
     });
-    return nn;
-  }, next_nodes);
-  const new_done = valid_next_nodes_with_cost.reduce((new_done, node) => {
-    const node_x = parseInt(node.split('_')[0]);
-    const node_y = parseInt(node.split('_')[1]);
-    done[node_x][node_y] = 1;
-    return done;
-  }, done);
-  return findPathWithCost(running_cost + 1, map, new_next_nodes, done);
+    return map_cost_nodes;
+  }, map_cost_nodes);
+
+const containsExit = (valid_next_nodes, cave) => valid_next_nodes.some(
+  ([node_x, node_y]) => node_x === cave.length - 1 && node_y === cave[0].length -1
+);
+
+const findExitCost = cave => {
+  let cost = 1;
+  let map_cost_nodes = cave[0][1] === cave[1][0] ? // { [cost]: [nodes with cost] }
+    ({ [cave[0][1]]: [[0,1], [1,0]] }) :
+    ({ [cave[0][1]]: [[0,1]], [cave[1][0]]: [[1,0]] });
+  let nodes_done = (new Array(cave.length)).fill(0) // Matrix filled with 0s
+    .map(() => new Array(cave.length).fill(0));
+  // Array of unvisited nodes with given cost
+  let next_nodes = map_cost_nodes[cost] || [];
+  while (!containsExit(next_nodes, cave)) {
+    if (next_nodes.length) {
+      map_cost_nodes = addFoundNodes(map_cost_nodes, next_nodes, cave, nodes_done, cost);
+      next_nodes.forEach(([node_x, node_y]) => {
+        nodes_done[node_x][node_y] = 1;
+      });
+    }
+    cost += 1;
+    next_nodes = (map_cost_nodes[cost] || [])
+      .filter(([node_x, node_y]) => !nodes_done[node_x][node_y]);
+  }
+  return cost;
 };
 
-const initial_next_nodes = map[0][1] === map[1][0] ?
-  ({ [map[0][1]]: ['0_1', '1_0'] }) :
-  ({ [map[0][1]]: ['0_1'], [map[1][0]]: ['1_0'] });
-const initial_done = (new Array(map.length)).fill(0)
-  .map(() => new Array(map.length).fill(0));
-const result_A = findPathWithCost(1, map, initial_next_nodes, initial_done);
+// A
+const cave = (new Array(lines.length)).fill(0)
+  .map((row, i) => lines[i].split('').map(x => parseInt(x)));
+const result_A = findExitCost(cave);
 
 // B
-const lines_B = lines.map(l => l.split('').map(x => parseInt(x) - 1));
-const map_B = (new Array(5 * lines.length)).fill(0)
-  .map((row, i) => {
+const cave_B = (new Array(5 * lines.length)).fill(0).map((row, i) =>
+  (new Array(5 * lines.length)).fill(0).map((x, j) => {
     const tile_row_extra = Math.floor(i/lines.length);
-    return ([
-      ...lines_B[i%lines.length],
-      ...lines_B[i%lines.length].map(x => x+1),
-      ...lines_B[i%lines.length].map(x => x+2),
-      ...lines_B[i%lines.length].map(x => x+3),
-      ...lines_B[i%lines.length].map(x => x+4),
-    ]).map(x => (x + tile_row_extra)%9 + 1);
-  });
-const initial_next_nodes_B = map[0][1] === map[1][0] ?
-  ({ [map[0][1]]: ['0_1', '1_0'] }) :
-  ({ [map[0][1]]: ['0_1'], [map[1][0]]: ['1_0'] });
-const initial_done_B = (new Array(5*map.length)).fill(0)
-  .map(() => new Array(5*map.length).fill(0));
-const result_B = findPathWithCost(1, map_B, initial_next_nodes, initial_done_B);
+    const tile_col_extra = Math.floor(j/lines.length);
+    return (cave[i%lines.length][j%lines.length] + tile_row_extra + tile_col_extra - 1)%9 + 1;
+  })
+);
+const result_B = findExitCost(cave_B);
